f1a8b3c2645806cd6a73ea9e2f785a725590bac2
Sam Bzez <sam@bzez.co>
2017-11-08 14:53:37 +0100

    First Upload

---
+++ assets/php/sendemail.php
@@ -0,0 +1,22 @@
+<?php
+
+	$name = @trim(stripslashes($_POST['name'])); 
+	$email = @trim(stripslashes($_POST['email']));  
+	$subject = @trim(stripslashes($_POST['subject']));  
+	$message = @trim(stripslashes($_POST['message'])); 
+
+	$email_from = $email;
+	$email_to = 'indabzez@icloud.com'; //replace with your email
+
+	$body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
+
+	$success = @mail($email_to, $body, 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message);
+	
+?>
+
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<script>alert("Merci d'avoir pris contact ! Je vous répondrais au plus vite...");</script>
+	<meta HTTP-EQUIV="REFRESH" content="0; url=http://samirm.e2simplon.fr/me"> 
+</head>
